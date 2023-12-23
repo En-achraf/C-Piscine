f82ab2da0b0066cec170fd398f29f977793bc87d
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2023-12-23 13:08:30 +0100

    nice\

--- C01/ex04/ft_ultimate_div_mod.c
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -4,27 +4,21 @@ void    ft_ultimate_div_mod(int *a, int *b) {
    
    int div;
    int mod;
-    if ( *a != '0' && *b != '0') {
-        div = 0;
-        mod = 0;
-    }
-    else {
-        div = *a / *b;
-        mod = *a % *b;
-        *a  = div;
-        *b = mod;
-    }
+    
+    div = *a / *b;
+    mod = *a % *b;
+    *a  = div;
+    *b = mod;
 }
-/*
+
 int main(void) {
     int a; 
     int b;
-    a = 1;
-    b = 0;
+    a = 0;
+    b = 1;
 
     ft_ultimate_div_mod(&a, &b);
 
     printf("the div ruselt : %d and the mod result : %d",a, b);
     return 0;
 }
-*/
