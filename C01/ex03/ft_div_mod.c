1298e86fdcc4b7341610dec0d89995811f72386e
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2023-12-23 13:09:02 +0100

    nice

--- C01/ex03/ft_div_mod.c
+++ C01/ex03/ft_div_mod.c
@@ -2,16 +2,10 @@
 
 void    ft_div_mod(int a,int b,int *div,int *mod) {
 
-    if ( a != '0' && b != '0') {
-        *div = 0;
-        *mod = 0;
-    }
-    else {
-        *div = a/b;
-        *mod = a%b;
-    }
+    *div = a/b;
+    *mod = a%b;
 }
-/*
+
 int main(void) {
     int div, mod ,a ,b;
     a = 2;
@@ -22,4 +16,3 @@ int main(void) {
     printf("the div ruselt : %d and the mod result : %d", div, mod);
     return 0;
 }
-*/
