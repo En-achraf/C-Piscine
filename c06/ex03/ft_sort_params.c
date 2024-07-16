85e82561e96e693fdb4fb2b3e5ea28b98eda6802
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-07-16 18:41:14 +0100

    c06
    good luck

---
+++ c06/ex03/ft_sort_params.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/09 04:36:57 by acennadi          #+#    #+#             */
+/*   Updated: 2024/07/09 06:36:19 by acennadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	ft_sort_params(int ac, char *av[])
+{
+	char	*swap;
+	int		i;
+
+	i = 1;
+	while (i < ac - 1)
+	{
+		if (ft_strcmp(av[i], av[i + 1]) > 0)
+		{
+			swap = av[i];
+			av[i] = av[i + 1];
+			av[i + 1] = swap;
+			i = 0;
+		}
+		i++;
+	}
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		write(1, &str[i++], 1);
+	write(1, "\n", 1);
+}
+
+int	main(int ac, char *av[])
+{
+	int	i;
+
+	i = 1;
+	ft_sort_params(ac, av);
+	while (i < ac)
+		ft_putstr(av[i++]);
+	return (0);
+}
