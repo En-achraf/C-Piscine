fdf25c1827e0b1c479f040ff0aa3493dbf8a003e
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-09-11 23:24:16 +0100

    Done

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -1,33 +1,28 @@
 #include <unistd.h>
 
-void ft_putchar(char c) {
-    write(1,&c,1);
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
 
-void    ft_putnbr(int nb) {
+void	ft_putnbr(int n)
+{
+	unsigned int nb;
 
-    if(nb == -2147483648) {
-        ft_putchar('-');
-        ft_putnbr(2);
-        ft_putnbr(147483648);
-    }
-    else if(nb < 0){
-        nb = -nb;
-        ft_putchar('-');
-        ft_putnbr(nb);
-    }
-    else if(nb >= 0){
-        if ((nb == 0 || nb <=9)){
-            ft_putchar(nb + 48);
-        }
-        else if(nb > 9){
-            ft_putnbr(nb / 10);
-            ft_putnbr(nb % 10);
-        }
-    }
-}
+	nb = n;
+	if (n < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
 
-int main(void) {
-    ft_putnbr(2147483647);
-    return 0;
+	if (nb < 10)
+	{
+		ft_putchar(nb + '0');
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
 }
